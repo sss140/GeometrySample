fc69bb0144db92c1e1aba5217798390071f6b1e5
佐藤一成 <kazunarisato0309@gmail.com>
2020-04-21 16:49:32 +0900

    Initial Commit

---
+++ GeometrySample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometrySample
+//
+//  Created by 佐藤一成 on 2020/04/21.
+//  Copyright © 2020 s140. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
